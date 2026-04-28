68c3fcd27b3b62649a11cffd73d746ee0755a05c
FlorianUni2025 <s4flbaur@uni-trier.de>
2026-04-28 21:07:50 +0200

    GUI Canvas

--- src/main/java/com/example/sokoban_project/GUI.java
+++ src/main/java/com/example/sokoban_project/GUI.java
@@ -5,26 +5,34 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GUI {
 
-    private GridPane root = new GridPane();
+    private StackPane root = new StackPane();
     private FeldButton[][] buttons;
     private Controller con;
+    Canvas canvas = new Canvas(800, 600);
+
+    GraphicsContext gc = canvas.getGraphicsContext2D();
 
     public GUI() {
+        gc.fillText("Hallo Welt", 100, 100);
+        root.getChildren().add(canvas);
     }
 
     public void setController(Controller c) {
         this.con = c;
     }
 
-    public GridPane getRoot() {
+    public StackPane getRoot() {
         return root;
     }
 }
