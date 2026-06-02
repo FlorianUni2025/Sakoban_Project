f2a95833396eb8e6cbe152a91180c5f235a680cd
Florian Baur <s4flbaur@uni-trier.de>
2026-06-02 20:07:03 +0200

    Refactor: Update MVC structure to match current master branch\n\n- Fixed player position handling to use int[] array matching master\n- Adjusted setPlayerPosition to update array elements\n- Maintained separation of concerns between Controller, GameLogic, and Renderer\n- Compatible with Crates and Goal entity types from master branch

--- src/main/java/com/example/sokoban_project/GameState.java
+++ src/main/java/com/example/sokoban_project/GameState.java
@@ -12,8 +12,8 @@ public class GameState {
     private int col;
     private int row;
     private int levelId;
-    private int playerX;
-    private int playerY;
+    private int[] player;
+
 
     GameState(int col, int row)  {
         this.col = col;
@@ -41,8 +41,7 @@ public class GameState {
         this.levelId = level.getId();
         this.col = level.getWidth();
         this.row = level.getHeight();
-        this.playerX = level.getPlayerX();
-        this.playerY = level.getPlayerY();
+        this.player = level.getPlayer();
         this.field = level.getGameField();
 
         Entity e = field[0][0];
@@ -53,8 +52,8 @@ public class GameState {
      * Updates the player position
      */
     public void setPlayerPosition(int x, int y) {
-        this.playerX = x;
-        this.playerY = y;
+        this.player[0] = x;
+        this.player[1] = y;
     }
 
     /**
@@ -65,12 +64,14 @@ public class GameState {
     }
 
     public int getPlayerX() {
-        return playerX;
+        return player[0];
     }
 
     public int getPlayerY() {
-        return playerY;
+        return player[1];
     }
+
+
 
     public void updateField(BiPredicate<Integer, Integer> con, BiConsumer<Integer, Integer> action){
         for(int i=0; i<col; i++){
@@ -106,4 +107,5 @@ public class GameState {
     public int getRow() {
         return row;
     }
+
 }
